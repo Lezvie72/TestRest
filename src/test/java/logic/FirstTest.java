596acbec884b3362058d94526e8b7b805f221ce1
Mark100F <seregin.mi@unicom24.ru>
2020-04-22 13:39:24 +0300

    Первая версия фреймворка

--- src/test/java/logic/FirstTest.java
+++ src/test/java/logic/FirstTest.java
@@ -1,17 +1,37 @@
 package logic;
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.JSONTokener;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import static io.restassured.RestAssured.given;
 
 public class FirstTest {
 
 
-    public JSONObject createInCorrectJSON(Object content, Object due_date, Object due_lang, Object priority) {
-        JSONObject json = new JSONObject();
-        json.put("content", content);
-        json.put("due_string", due_date);
-        json.put("due_lang", due_lang);
-        json.put("priority", priority);
-        return json;
+    public static JSONObject test(String jsonFileName) {
+        String baseDir = System.getProperty("user.dir");
+        String jsonDir = baseDir + "/src/test/java/jsons/";
+        File file = new File(jsonDir + jsonFileName + ".json");
+        try {
+            return (JSONObject) readJsonSimpleDemo(file);
+        }
+        catch (Exception ignored) {
+
+        }
+        return null;
+    }
+
+    public static Object readJsonSimpleDemo(File file) throws Exception {
+        file.createNewFile();
+        FileReader reader = new FileReader(file);
+        JSONParser jsonParser = new JSONParser();
+        return jsonParser.parse(reader);
     }
 
     public void sendRequestAndCheckStatus(String url, int code, JSONObject jsonObject) {
--- src/test/java/stepDef/testStepDef.java
+++ src/test/java/stepDef/testStepDef.java
@@ -1,32 +1,27 @@
 package stepDef;
+import cucumber.api.DataTable;
 import cucumber.api.java.ru.Когда;
 import cucumber.api.java.ru.Тогда;
 import logic.FirstTest;
 
+import java.util.List;
+import java.util.Map;
+
 public class testStepDef {
 
-//TODO: сделать передачу параметрами из сценария
-
     FirstTest firstTest = new FirstTest();
+    String nameOfJson = null;
 
-    @Когда("^выполнен POST запрос на URL \"([^\"]*)\". Ожидается код ответа: (\\d+)$")
-    public void sendRequest(String url, int code){
-        firstTest.sendRequestAndCheckStatus(url, code, firstTest.createInCorrectJSON("MyTest", "tomorrow at 12:00", "en", 4));
+    @Когда("^выполнен POST запрос на URL \"([^\"]*)\" с параметрами из таблицы. Ожидается код ответа: (.*)$")
+    public void sendRequest(String url, int code, DataTable arg){
+        List<List<String>> table = arg.asLists(String.class);
+        for (int i=0; i<table.size(); i++) {
+            switch (table.get(i).get(0)){
+                case ("BODY"):
+                    nameOfJson = table.get(i).get(2);
+                    break;
+            }
+        }
+        firstTest.sendRequestAndCheckStatus(url, code, FirstTest.test(nameOfJson));
     }
-
-    @Когда("^выполнен POST запрос на URL \"([^\"]*)\" с параметрами. Ожидается код ответа: (\\d+)$")
-    public void sendRequestWithParam(String url, int code){
-        firstTest.sendRequestAndCheckStatus(url, code, firstTest.createInCorrectJSON("MyTest", "tomorrow at 12:00", "en", "4"));
-    }
-
-    @Когда("^выполнен POST запрос на URL \"([^\"]*)\" с одним отсутствующим параметром. Ожидается код ответа: (\\d+)$")
-    public void sendRequestWithoutParam(String url, int code){
-        firstTest.sendRequestAndCheckStatus(url, code, firstTest.createInCorrectJSON("", "tomorrow at 12:00", "en", 4));
-    }
-
-    @Когда("^выполнен POST запрос на URL \"([^\"]*)\" без токена. Ожидается код ответа: (\\d+)$")
-    public void sendRequestWithoutToken(String url, int code){
-        firstTest.sendRequestWithotToken(url, code, firstTest.createInCorrectJSON("MyTest", "tomorrow at 12:00", "en", 4));
-    }
-
 }
